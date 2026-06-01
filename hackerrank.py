2ea59f3922fe11b87520717e5e184f1c6cd205ef
saidurgabanuru04 <saibanuru1234@gmail.com>
2026-06-01 17:08:00 +0530

    conditional statements

---
+++ hackerrank.py
@@ -0,0 +1,20 @@
+n = int(input( "Enter a number: "))
+
+if ( n%2!=0):
+    print ("Weird")
+    
+    
+else:
+
+    if ( 2 <= n <= 5):
+        print("Not Weird")
+        
+    else: 
+         
+        if ( 6 <= n <=20):
+             print("Weird")
+             
+        else:
+         
+            if n > 20:
+                      print("Not Weird")
