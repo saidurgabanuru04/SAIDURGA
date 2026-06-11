8c9936bca0f9113c6b7dd5f38746f2a613252d79
saidurgabanuru04 <saidurgabanuru1504@gmail.com>
2026-06-11 20:42:56 +0530

    leap year

---
+++ leap_year.py
@@ -0,0 +1,12 @@
+def is_leap(year):
+    leap = True
+    not_leap = False
+    if( year%4==0) :
+        if  year%100 ==0 and not year%400==0:
+            return not_leap
+        else:
+            return leap
+    else:
+        return not_leap
+year = int(input("enter the year:"))
+print(is_leap(year))
